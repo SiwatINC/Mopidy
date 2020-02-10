33f92df8410021dc392375bbd6b1b2d8fb9c954d
Siwat Sirichai <siwat2545@cud55.com>
2020-02-10 12:05:34 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -2,11 +2,8 @@ echo Siwat INC Mopidy+SnapCast Container Initializing
 echo "Starting Container Initialization Script."
 if [ -z "$(ls -A /config)" ]; then
    echo "FIRSTRUN Initializing . . ."
-   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/nginx.conf" > /config/nginx/nginx.conf
-   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/fastcgi_params" > /config/nginx/fastcgi_params
-   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/mime.types" > /config/nginx/mime.types
-   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/default" > /config/nginx/site-confs/default
-   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/index.php" > /config/www/index.php
+   curl "https://raw.githubusercontent.com/SiwatINC/Mopidy/master/config/mopidy.conf" > /config/mopidy.conf
+   curl "https://raw.githubusercontent.com/SiwatINC/Mopidy/master/config/snapcast.conf" > /config/snapcast.conf
    exit 0
 else
    echo "Already Initiated, Starting Mopidy and SnapServer Daemon"
